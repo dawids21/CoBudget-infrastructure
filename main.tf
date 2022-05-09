65db10a6d58e95a6946fdaa0113c751c34539dfc
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-05-09 20:37:46 +0200

    set variables for vercel in root module

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 module "vercel_cobudget_frontend" {
   source = "./modules/vercel-cobudget-frontend"
+
+  okta_uri          = var.vercel_okta_uri
+  okta_client_id    = var.vercel_okta_client_id
+  okta_redirect_uri = var.vercel_okta_redirect_uri
+  backend_url       = var.vercel_backend_url
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,26 @@ variable "vercel_api_token" {
   type        = string
   sensitive   = true
 }
+
+variable "vercel_okta_uri" {
+  description = "Okta OIDC URI"
+  type        = string
+  sensitive   = true
+}
+
+variable "vercel_okta_client_id" {
+  description = "Okta OIDC Client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "vercel_okta_redirect_uri" {
+  description = "Okta OIDC redirect URI"
+  type        = string
+  sensitive   = true
+}
+
+variable "vercel_backend_url" {
+  description = "Backend app url"
+  type        = string
+}
