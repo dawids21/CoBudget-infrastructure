57f6747f06eaf3f14c65764e2c5bb269d2bc6e34
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-05-11 21:35:44 +0200

    specify required resources

---
+++ modules/heroku-cobudget-backend/main.tf
@@ -0,0 +1,5 @@
+// heroku_addon for database
+// heroku_app for app
+// heroku_config for config vars
+// heroku_app_config_association for linking app with config vars
+// heroku_formation for creating dyno
