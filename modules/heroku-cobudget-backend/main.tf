defc5f313566a0304b5b550d33c4a6c6c6883d2e
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-05-11 21:45:35 +0200

    create heroku_config resource

--- modules/heroku-cobudget-backend/main.tf
+++ modules/heroku-cobudget-backend/main.tf
@@ -3,3 +3,11 @@
 // heroku_config for config vars
 // heroku_app_config_association for linking app with config vars
 // heroku_formation for creating dyno
+
+resource "heroku_config" "backend_config_vars" {
+  vars = {
+    "APP_PORT"     = "8080"
+    "CORS_ORIGINS" = var.frontend_url
+    "OAUTH_ISSUER" = var.oauth_issuer
+  }
+}
---
+++ modules/heroku-cobudget-backend/variables.tf
@@ -0,0 +1,9 @@
+variable "frontend_url" {
+  description = "URL of the frontend app"
+  type        = string
+}
+
+variable "oauth_issuer" {
+  description = "Issuer URL for OAuth2"
+  type        = string
+}
