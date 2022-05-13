955750851ecbe12f9a9ca915fc8d59c5d943d66d
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-05-13 13:45:27 +0200

    rename app

--- modules/heroku-cobudget-backend/main.tf
+++ modules/heroku-cobudget-backend/main.tf
@@ -1,6 +1,14 @@
 resource "heroku_app" "backend" {
-  name   = "heroku-test"
+  name   = "dawids21-heroku-test"
   region = "eu"
+}
+
+resource "heroku_build" "backend" {
+  app_id = heroku_app.backend.id
+
+  source {
+    path = "../backend"
+  }
 }
 
 resource "heroku_config" "backend_config_vars" {
@@ -22,10 +30,13 @@ resource "heroku_formation" "backend" {
   type     = "web"
   quantity = 1
   size     = "free"
+  depends_on = [
+    heroku_build.backend
+  ]
 }
 
 resource "heroku_addon" "backend-database" {
   app_id = heroku_app.backend.id
-  plan   = "hobby-dev"
+  plan   = "heroku-postgresql:hobby-dev"
   name   = "cobudget-backend-database"
 }
