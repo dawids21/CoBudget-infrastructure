e7f5db35211894b414905d8e598ec53d2990d3aa
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-05-13 13:35:19 +0200

    reorder variables

--- variables.tf
+++ variables.tf
@@ -4,21 +4,6 @@ variable "vercel_api_token" {
   sensitive   = true
 }
 
-variable "vercel_okta_uri" {
-  description = "Okta OIDC URI"
-  type        = string
-}
-
-variable "vercel_okta_client_id" {
-  description = "Okta OIDC Client ID"
-  type        = string
-}
-
-variable "vercel_okta_redirect_uri" {
-  description = "Okta OIDC redirect URI"
-  type        = string
-}
-
 variable "heroku_email" {
   description = "Email to authenticate Heroku API"
   type        = string
@@ -33,5 +18,20 @@ variable "heroku_api_key" {
 
 variable "app_domain" {
   description = "Domain of the frontend app"
+  type        = string
+}
+
+variable "vercel_okta_uri" {
+  description = "Okta OIDC URI"
+  type        = string
+}
+
+variable "vercel_okta_client_id" {
+  description = "Okta OIDC Client ID"
+  type        = string
+}
+
+variable "vercel_okta_redirect_uri" {
+  description = "Okta OIDC redirect URI"
   type        = string
 }
