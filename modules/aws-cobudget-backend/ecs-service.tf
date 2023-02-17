55b4e9b63777ffcd7b8c34a6a8efa8ae9f403280
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-02-17 17:21:01 +0100

    feat(IaaS): Add IAM role for task to upload to S3

--- modules/aws-cobudget-backend/ecs-service.tf
+++ modules/aws-cobudget-backend/ecs-service.tf
@@ -1,5 +1,6 @@
 resource "aws_ecs_task_definition" "cobudget" {
   execution_role_arn    = aws_iam_role.ecs_task_execution.arn
+  task_role_arn         = aws_iam_role.cobudget_task_role.arn
   container_definitions = jsonencode([
     {
       essential    = true
